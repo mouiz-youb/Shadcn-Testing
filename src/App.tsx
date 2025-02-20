9d06379ccdc5682131cfb158327238961edec09e
abdelmouiz <a_youb@estin.dz>
2025-02-20 08:34:41 -1100

    the setup of the project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+function App() {
+  return (
+    <div className="bg-blue-500 text-3xl w-screen h-screen">
+      <h1>hello world</h1>
+      <div className="w-1/2 flex justify-center items-center  text-green-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        obcaecati ipsam voluptatem, possimus vero, incidunt officia ipsa
+        provident ratione eaque impedit sequi consequuntur natus dignissimos
+        perferendis adipisci porro accusamus eligendi.
+      </div>
+    </div>
+  );
+}
+
+export default App;
